0668d8abcf1d8beea7b1bbf1c00295b74d6e9563
Mateus Rossi <rossijf2@gmail.com>
2022-08-09 17:51:41 -0300

    Initial commit
    Application body.

---
+++ scbapi/src/main/java/com/rfbike/scbapi/ScbapiApplication.java
@@ -0,0 +1,13 @@
+package com.rfbike.scbapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScbapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScbapiApplication.class, args);
+    }
+
+}
---
+++ scbapi/src/test/java/com/rfbike/scbapi/ScbapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rfbike.scbapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScbapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
